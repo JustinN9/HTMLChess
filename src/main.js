d75a5e9c86db0126f47e65fd4bcc243c4caf6294
JustinN9 <nortonj9@mymacewan.ca>
2026-02-11 12:26:16 -0700

    Basic piece movement added
    Currently just in the main file, will move to board or move. Additionally, currently removes any piece that is on the move square, even if it is itself. Additionally, there is no move checkers or validators. However, basic select piece and move to functionallity works and shows which square is selected.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,42 @@ import { initialBoard, renderBoard } from './board.js';
 
 const boardContainer = document.getElementById('board');
 
-renderBoard(boardContainer, initialBoard);
+let board = initialBoard;
+let selectedPiece = null;
+let selectedPos = null;
+
+renderBoard(boardContainer, board);
+addClickHandlers();
+
+function addClickHandlers() {
+  boardContainer.addEventListener("click", onSquareClick);
+}
+
+function onSquareClick(e) {
+  const square = e.target.closest(".square");
+  if (!square) return;
+
+  const row = parseInt(square.dataset.row);
+  const col = parseInt(square.dataset.col);
+
+  const piece = board[row][col];
+
+  //Select square
+  if (!selectedPiece) {
+    if (piece) {
+      selectedPiece = piece;
+      selectedPos = { row, col };
+      square.classList.add("selected");
+    }
+  } 
+  //Move square
+  else {
+    board[row][col] = selectedPiece;
+    board[selectedPos.row][selectedPos.col] = null;
+
+    selectedPiece = null;
+    selectedPos = null;
+
+    renderBoard(boardContainer, board);
+  }
+}
